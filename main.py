7563bfc24caeb5ba7f85829fefb50eaaf9749f5e
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-04-16 18:42:20 +0100

    done

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from dotenv import load_dotenv
 import os
 import random
 
-
 load_dotenv()
 
 mastodon = Mastodon(
@@ -14,31 +13,24 @@ mastodon = Mastodon(
     api_base_url="https://mastodon.social"
 )
 
-#get account id
-followering = mastodon.account_following(110209472762362718)
-with open("jokes.txt", "r") as f:
-    jokes = f.readlines()
-
+following = mastodon.account_following(110209472762362718)
 
 followering_posts = []
-
-for i in followering:
-    for post in mastodon.account_statuses(i):
-        followering_posts.append(post.id)
+for i in following:
+    for posts in mastodon.account_statuses(i):
+        followering_posts.append(posts.id)
 
 print(followering_posts)
 
+with open("jokes.txt", "r") as f:
+    jokes = f.readlines()
 
 while True:
-    #check if there are new posts
-    for i in followering:
-        for post in mastodon.account_statuses(i):
-            if post.id not in followering_posts:
-                followering_posts.append(post.id)
-                print("New post")
-                selected_joke = random.choice(jokes)
-                mastodon.status_post(selected_joke, in_reply_to_id=post.id)
+    for i in following:
+        for posts in mastodon.account_statuses(i):
+            if posts.id not in followering_posts:
+                followering_posts.append(posts.id)
+                joke_used = random.choice(jokes)
+                mastodon.status_post(joke_used,in_reply_to_id=posts.id)
 
-    time.sleep(60)
-
-    
+    time.sleep(120)
