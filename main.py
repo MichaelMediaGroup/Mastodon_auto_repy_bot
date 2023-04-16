f0da123ec8affa78fc7b924321fa7cef2e39b62f
micfun123 <54761507+micfun123@users.noreply.github.com>
2023-04-16 17:36:10 +0100

    done

---
+++ main.py
@@ -0,0 +1,34 @@
+from mastodon import Mastodon
+import asyncio
+import time
+from dotenv import load_dotenv
+import os
+import random
+
+
+load_dotenv()
+
+mastodon = Mastodon(
+    client_id=os.getenv("Client_key"),
+    client_secret=os.getenv("Client_secret"),
+    access_token=os.getenv("access_token"),
+    api_base_url="https://mastodon.social"
+)
+
+#get account id
+followering = mastodon.account_following(110090602223960345)
+with open("jokes.txt", "r") as f:
+    jokes = f.readlines()
+
+jokes = []
+
+while True:
+    #if a follower creats a new post
+    for follower in followering:
+        follower_posts = mastodon.account_statuses(follower.id)
+        for post in follower_posts:
+            if post.favourited == False:
+                #like the post
+                mastodon.status_favourite(post.id)
+                #reply to the post
+                mastodon.status_post("@" + post.account.acct + " " + random.choice(["Hello", "Hi", "Hey", "Greetings"]), in_reply_to_id=post.id)
